2302dcf86e15878788e6096fa5f153292fe640d5
Bel <79281901+Bebebole@users.noreply.github.com>
2024-06-06 21:55:31 -0700

    echo current action

--- Scripts/fix_ui_and_screen_issues.sh
+++ Scripts/fix_ui_and_screen_issues.sh
@@ -1,10 +1,14 @@
 SCRIPT_PATH="${BASH_SOURCE[0]%/*}"
 
 #Change screen color profile
+echo -n "Changing the screen color profile..."
 DISPLAY_PROFILE_PATH="$(ls /Library/ColorSync/Profiles/Displays/** | grep "Nameless Display" | head -1)"
 $SCRIPT_PATH/customdisplayprofiles-intel set "$DISPLAY_PROFILE_PATH"
+echo " Done."
 
 #Adjust Launchpad icons size
+echo -n "Adjusting Launchpad icons size..."
 defaults write com.apple.dock springboard-columns -int 6
 defaults write com.apple.dock springboard-rows -int 4
 killall Dock
+echo " Done."
